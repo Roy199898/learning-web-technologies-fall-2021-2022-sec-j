97471d7ffb7ed88675c9734a7c59cdf763fb7897
Roy199898 <dipr19989@gmail.com>
2021-09-29 21:46:49 +0600

    modified

--- MID_LAB_TASK_PHP_FORM/Task_3/A/DateOfBirth.php
+++ MID_LAB_TASK_PHP_FORM/Task_3/A/DateOfBirth.php
@@ -1,5 +1,22 @@
 <?php
 
-echo "Date of Birth: ".$_POST['day']."/".$_POST['month']."/".$_POST['year'];
+if(isset($_REQUEST['submit'])){
+    
+    if($_REQUEST['day']=="" or $_REQUEST['month']=="" or $_REQUEST['year']==""){
+        
+        echo "Please input all the fields";
+        
+    }
+    
+    else{
+        
+        
+        echo "Date of Birth ".$_REQUEST['day']."/".$_REQUEST['month']."/".$_REQUEST['year'];
+        
+    }
+    
+    
+}
+
 
 ?>
