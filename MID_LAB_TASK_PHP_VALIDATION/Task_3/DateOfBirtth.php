3e5fb4d093697e3f1281342ae01a0040471fec6f
Roy199898 <dipr19989@gmail.com>
2021-10-04 15:50:16 +0600

    DateOfBirth Validation

--- MID_LAB_TASK_PHP_VALIDATION/Task_3/DateOfBirtth.php
+++ MID_LAB_TASK_PHP_VALIDATION/Task_3/DateOfBirtth.php
@@ -1,55 +1,54 @@
-<?php
+<html>
 
+<head>
+    <title>Date Of Birth</title>
+</head>
 
-    if(isset($_REQUEST['submit'])){
-        $day = $_REQUEST['day'];
-        $month = $_REQUEST['month'];
-        $year = $_REQUEST['year'];
+<body>
+    <form method="get" action="#">
+        <fieldset>
+            <section>Date Of Birth</section>
+            <table>
+                <tr>
+                    <td align="center">dd</td>
+                    <td align="center">mm</td>
+                    <td align="center">yyyy</td>
+                </tr>
+                <tr>
+                    <td>
+                        <input size="2" type="text" name="day"> /
+                    </td>
+                    <td>
+                        <input size="2" type="text" name="month"> /
+                    </td>
+                    <td>
+                        <input size="4" type="text" name="year">
+                    </td>
+                </tr>
+            </table>
+            <hr />
+            <input name="submit" type="submit" value="Submit">
+            <p>
+                <?php
+                if (isset($_REQUEST['submit'])) {
+                    $day = $_REQUEST['day'];
+                    $month = $_REQUEST['month'];
+                    $year = $_REQUEST['year'];
 
-        $date = $day.'/'.$month.'/'.$year;
+                    $date = $day . '/' . $month . '/' . $year;
 
-        if($day!="" && $month!="" && $year!=""){
-            echo $date;
-        }
-        else{
-            echo 'Invalid Date...';
-        }
-    }
+                    if ($day == "" || $month == "" || $year == "" || $day < 1 || $day > 31 || $month < 1 || $month > 12 || $year < 1953 || $year > 1998) {
+                        echo $date . ' is not a valid date';
+                        return;
+                    }
 
-?>
+                    echo $date . ' submitted';
+                }
+                ?>
 
-    <html>
+            </p>
+        </fieldset>
+    </form>
+</body>
 
-    <head>
-        <title>Date Of Birth</title>
-    </head>
-
-    <body>
-        <form method="get" action="#">
-            <fieldset>
-                <section>Date Of Birth</section>
-                <table>
-                    <tr>
-                        <td align="center">dd</td>
-                        <td align="center">mm</td>
-                        <td align="center">yyyy</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <input size="2" type="text" name="day"> /
-                        </td>
-                        <td>
-                            <input size="2" type="text" name="month"> /
-                        </td>
-                        <td>
-                            <input size="4" type="text" name="year">
-                        </td>
-                    </tr>
-                </table>
-                <hr/>
-                <input name="submit" type="submit" value="Submit">
-            </fieldset>
-        </form>
-    </body>
-
-    </html>
+</html>
