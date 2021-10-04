99f7a4d6a6c610973aedbf9ce559a878dee5ac7b
Roy199898 <dipr19989@gmail.com>
2021-10-04 15:31:51 +0600

    emailValidation

--- MID_LAB_TASK_PHP_VALIDATION/Task_2/Email.php
+++ MID_LAB_TASK_PHP_VALIDATION/Task_2/Email.php
@@ -1,19 +1,4 @@
-<?php
-
-//print_r($_GET);
-
-if (isset($_REQUEST['submit'])) {
-	$email = $_REQUEST['email'];
-	if ($email == "") {
-		echo "null value...";
-	} else {
-		echo $email;
-	}
-}
-
-?>
-
-<!DOCTYPE html>
+]<!DOCTYPE html>
 <html>
 
 <head>
@@ -28,7 +13,49 @@ if (isset($_REQUEST['submit'])) {
             <input type="text" name="email" value="">
             <hr>
             <input type="submit" name="submit" value="Submit">
+            <p>
+                <?php
 
+
+                function isValidEmail($email)
+                {
+                    $indexOfA = -1;
+                    $indexOfDot = -1;
+                    for ($i = 0; $i < strlen($email); ++$i) {
+                        if ($email[$i] == '@') {
+                            $indexOfA = $i;
+                        }
+                        if ($email[$i] == '.') {
+                            $indexOfDot = $i;
+                        }
+                    }
+
+                    if ($indexOfA == -1 || $indexOfDot == -1) {
+                        return false;
+                    }
+
+                    if ($indexOfA < 1 || $indexOfDot == strlen($email) - 1 || $indexOfDot - $indexOfA < 2) {
+                        return false;
+                    }
+                    return true;
+                }
+
+                if (isset($_REQUEST['submit'])) {
+                    $email = $_REQUEST['email'];
+                    if ($email == "") {
+                        echo "Email can not be empty";
+                        return;
+                    }
+
+                    if(!isValidEmail($email)){
+                        echo $email.' is not valid';
+                        return;
+                    }
+
+                    echo $email.' submitted successfully';
+                }
+                ?>
+            </p>
         </fieldset>
     </form>
 </body>
