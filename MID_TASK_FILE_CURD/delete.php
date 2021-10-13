43fe5990e6b3d066b54628c8a4325f43c646cf34
Roy199898 <dipr19989@gmail.com>
2021-10-14 00:35:58 +0600

    fileCurd

---
+++ MID_TASK_FILE_CURD/delete.php
@@ -0,0 +1,34 @@
+
+<?php
+$id = $_GET['id'];
+if (rename('user.txt', 'userBac.txt') != 1) {
+	  echo 'Error backing up';
+	  return;
+      
+}
+      $myBacfile = fopen('userBac.txt', 'r');
+      $myfile = fopen('user.txt', 'w');
+      $counter = 0;
+
+     while (!feof($myBacfile)) {
+        	$data = fgets($myBacfile);
+	      if ($data != "") {
+		$user = explode('|', $data);
+
+		if (++$counter != $id) {
+			$userData = $user[0] . '|' . $user[1] . '|' . $user[2];
+			fwrite($myfile, $userData);
+		}
+	}
+}
+
+header('location: userlist.php');
+
+
+fclose($myfile);
+fclose($myBacfile);
+$myBacfile = fopen('userBac.txt','w');
+fwrite($myBacfile,"");
+fclose($myBacfile);
+
+?>
