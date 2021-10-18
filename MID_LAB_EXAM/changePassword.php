f091122574eb43e762ecfbd3d0cd5a12727a5ede
Roy199898 <dipr19989@gmail.com>
2021-10-18 16:34:46 +0600

    anothertask push

---
+++ MID_LAB_EXAM/changePassword.php
@@ -0,0 +1,18 @@
+<?php
+
+   session_start();
+if (isset($_COOKIE['isLoggedIn'])) {
+    if ($_COOKIE['isLoggedIn'] == 'true') {
+?>
+          <form action="handleChangePassword.php" method="post">
+
+          </form>
+<?php
+    } else {
+            header('location: login.php');
+    }
+} else {
+            header('location: login.php');
+}
+
+?>
