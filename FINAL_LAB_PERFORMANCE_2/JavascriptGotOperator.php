e696463acf607b1afb12d99c5bf505a1d9e4870d
Roy199898 <dipr19989@gmail.com>
2021-11-24 15:46:40 +0600

    second commit

--- FINAL_LAB_PERFORMANCE_2/JavascriptGotOperator.php
+++ FINAL_LAB_PERFORMANCE_2/JavascriptGotOperator.php
@@ -1,20 +1,22 @@
 <?php
+  $num1 = $_REQUEST['num1'];
+  $num2 = $_REQUEST['num2'];
+  $operator = $_REQUEST['operator'];
 
-	$num1 = $_REQUEST['num1'];
-	$num2 = $_REQUEST['num2'];
-	$operator = $_REQUEST['operator'];
-
-	switch($operator){
-	case '+':
+switch ($operator) {
+	case 'add':
 		 echo (float)$num1 + (float)$num2;
-		 break;
-	case '-':
-		 echo (float)$num1 - (float)$num2;
-		 break;
-	case '*':
-		 echo (float)$num1 * (float)$num2;
-		 break;
-	case '/':
+		break;
+	case 'sub':
+		echo (float)$num1 - (float)$num2;
+		break;
+	case 'mul':
+		echo (float)$num1 * (float)$num2;
+		break;
+	case 'div':
 		echo (float)$num1 / (float)$num2;
 		break;
-	}
+	default:
+		echo 'Invalid Operator';
+		break;
+}
